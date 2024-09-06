d9da5de9c33d01777aa5a6275012510b99657f0e
Lee Shao Herng <leeshaoherng@hotmail.com>
2024-09-06 18:12:20 +0800

    Add query functions

--- client.py
+++ client.py
@@ -1,3 +1,26 @@
 import mysql.connector as connector
 
-connection = connector.connect(user="root", password="password")
+# Task 1
+connection = connector.connect(user="root", password="password", db="LittleLemonDB")
+
+# Task 2
+show_tables_query = "SHOW tables"
+cursor = connection.cursor()
+cursor.execute(show_tables_query)
+results = cursor.fetchall()
+for result in results:
+    print(result)
+
+# Task 3
+query = """
+SELECT c.CustomerID, c.FullName, o.Cost
+FROM Customers c
+INNER JOIN Orders o ON c.CustomerID = o.CustomerID
+WHERE o.Cost > 60
+"""
+cursor.execute(query)
+results = cursor.fetchall()
+cols = cursor.column_names
+print(cols)
+for result in results:
+    print(result)
